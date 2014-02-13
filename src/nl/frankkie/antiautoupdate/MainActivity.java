5735f7fab9a8f8e59d503ad9036bdd8e409eca99
Frank Bouwens <frankkie12345@gmail.com>
2014-02-13 20:08:27 +0100

    Updated UI, added APK

--- src/nl/frankkie/antiautoupdate/MainActivity.java
+++ src/nl/frankkie/antiautoupdate/MainActivity.java
@@ -1,7 +1,11 @@
 package nl.frankkie.antiautoupdate;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -14,5 +18,18 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                try {
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this,"Please enable the Anti Auto Update Service.",Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
